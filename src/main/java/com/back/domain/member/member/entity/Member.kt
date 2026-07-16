7628ac388755ee0ac2aa8312bee1d352da3f72fe
EuniceNam <caellime@gmail.com>
2026-07-16 12:59:44 +0900

    refactor: entity를 kotlin으로 수정

--- src/main/java/com/back/domain/member/member/entity/Member.kt
+++ src/main/java/com/back/domain/member/member/entity/Member.kt
@@ -1,27 +1,16 @@
-package com.back.domain.member.member.entity;
+package com.back.domain.member.member.entity
 
-import com.back.global.jpa.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import com.back.global.jpa.entity.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
 
 @Entity
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-public class Member extends BaseEntity {
+class Member(username: String, password: String, nickname: String) : BaseEntity() {
     @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-
-    public Member(String username, String password, String nickname) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
-    }
+    var username: String = username
+        protected set
+    var password: String = password
+        protected set
+    var nickname: String = nickname
+        protected set
 }
--- src/main/java/com/back/global/jpa/entity/BaseEntity.kt
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.kt
@@ -1,27 +1,21 @@
-package com.back.global.jpa.entity;
+package com.back.global.jpa.entity
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
 @MappedSuperclass // 엔티티의 부모 클래스에는 이걸 달아야 한다.
-@EntityListeners(AuditingEntityListener.class)
-@Getter
-public abstract class BaseEntity {
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id = 0
+
     @CreatedDate
-    private LocalDateTime createDate;
+    val createDate: LocalDateTime? = null
+
     @LastModifiedDate
-    private LocalDateTime modifyDate;
+    val modifyDate: LocalDateTime? = null
 }
